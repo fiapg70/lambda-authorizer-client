02a63e6bbcf5d7a05f5f90624b1024f66d8bbc39
Rogério Fontes <fontestz@gmail.com>
2024-03-19 23:38:15 -0300

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -2,22 +2,22 @@ import boto3
 import json
 
 def lambda_handler(event, context):
-    body = json.loads(event['body'])
+    try:
+        body = json.loads(event['body'])
         if 'cpf' not in body or 'senha' not in body:
             raise ValueError('CPF e/ou senha ausentes no corpo da requisição')
-            
-    cpf = body['cpf']
-    senha = body['senha']
-    # Substitua esses valores pelos seus próprios
-    USER_POOL_ID = 'us-east-1_lLiNIC87U'
-    CLIENT_ID = '67v6o5suqcos03dd1pev1pb7bj'
-    USERNAME = cpf,
-    PASSWORD = senha,
 
-    # Inicializa o cliente Cognito
-    client = boto3.client('cognito-idp')
+        cpf = body['cpf']
+        senha = body['senha']
+        # Substitua esses valores pelos seus próprios
+        USER_POOL_ID = 'us-east-1_lLiNIC87U'
+        CLIENT_ID = '67v6o5suqcos03dd1pev1pb7bj'
+        USERNAME = cpf
+        PASSWORD = senha
 
-    try:
+        # Inicializa o cliente Cognito
+        client = boto3.client('cognito-idp')
+
         # Autentica o usuário no Amazon Cognito
         response = client.initiate_auth(
             AuthFlow='USER_PASSWORD_AUTH',
@@ -27,10 +27,11 @@ def lambda_handler(event, context):
             },
             ClientId="67v6o5suqcos03dd1pev1pb7bj"
         )
-        
+
         # Retorna o token de acesso se a autenticação for bem-sucedida
         if 'AuthenticationResult' in response:
             access_token = response['AuthenticationResult']['AccessToken']
+            print("Access Token:", access_token)  # Imprime o token de acesso
             return {
                 'statusCode': 200,
                 'body': json.dumps({'access_token': access_token})
